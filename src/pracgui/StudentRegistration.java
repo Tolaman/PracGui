ece2494fabab34485d7d7140d86d3df0b7107ffa
Michee Luzayisu <michemakaya11@gmail.com>
2026-05-16 15:55:20 +0200

    all the instance variable for the GUi

---
+++ src/pracgui/StudentRegistration.java
@@ -0,0 +1,19 @@
+
+package pracgui;
+
+/**
+ *
+ * @author micheeluzayisu
+ */
+
+import javax.swing.*;
+
+public class StudentRegistration {
+  private JLabel titleLbl, nameLbl, mobileLbl, studentLbl, addressLbl;
+  private JTextField nameTxt, mobileTxt;
+  private JTextArea displayTxtArea, addressTxtArea;
+  private JRadioButton undergradRad, postgradRad;
+  private JCheckBox acceptCheck;
+  private JButton submitBtn, resetBtn;
+  private JPanel mainPnl, formPnl, resultPnl;
+}
