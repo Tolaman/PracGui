fc523128e9c567c14e81f43f8dee89a2374e0b51
Michee Luzayisu <michemakaya11@gmail.com>
2026-05-16 16:44:28 +0200

    added the practicalHub object to run its GUi

--- src/pracgui/PracGui.java
+++ src/pracgui/PracGui.java
@@ -4,10 +4,16 @@ package pracgui;
  *
  * @author micheeluzayisu
  */
+
+import javax.swing.JFrame;
+
 public class PracGui {
 
   public static void main(String[] args) {
-    
+    PracticeHub frame = new PracticeHub();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(1000, 1000);
+    frame.setVisible(true);
   }
   
 }
